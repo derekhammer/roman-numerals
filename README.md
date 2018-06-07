9ba5086f10d7883b4c9555c6238f258071df0f53
Alex Bunnell and Derek Hammer <alex@>
2018-06-07 11:10:26 -0700

    Add functionallity for converting numbers in the hundreds and thousands, up to 3999.

--- README.md
+++ README.md
@@ -2,12 +2,12 @@ Specs:
 1. single digit number √
   input: 6
   output: VI
-2. double digit number
+2. double digit number √
   input: 17
   output: XVII
-3. triple digit numbers
+3. triple digit numbers √
   input: 451
   output: CDLI
-4. quad-dorgit numnums
+4. quad-dorgit numnums √
   input: 2849
   output: MMDCCCXLIX
--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,92 @@
 
 var romanNumerals = function(numeral)
 {
+
+
   var inputArray = numeral.split("").map(function(t){return parseInt(t)});
-  var onesArray = ["I","II","III","IV","V","VI","VII","VIII","IX"];
-  var tensArray = ["X","XX","XXX","XL","L","LX","LXX","LXXX","XC"];
-  var hundosArray = ["C","CC","CCC","CD","D","DC","DCC","DCCC","CM"];
-  var thousandsArray = ["M","MM","MMM"];
+  var limitBreak = parseInt(numeral);
+  var onesArray = ["","I","II","III","IV","V","VI","VII","VIII","IX"];
+  var tensArray = ["","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"];
+  var hundosArray = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"];
+  var thousandsArray = ["","M","MM","MMM"];
 
-
-  if (inputArray.length === 1)
+  if (limitBreak < 4000 || (inputArray.length > 4))
   {
-    var results = onesArray[(inputArray - 1)];
-    alert(results);
+    if (inputArray.length === 1) //one character long
+    {
+      var results = onesArray[(inputArray)];
+      return results;
+    }
+    else if (inputArray.length === 2) //two characters
+    {
+      for (var index = 0; index < inputArray.length; index++)
+      {
+        if (index === 0)
+        {
+          var doubleTens = tensArray[(inputArray[0])];
+        }
+        else if (index === 1)
+        {
+          var doubleOnes = onesArray[(inputArray[1])];
+        }
+      }
+      var results = doubleTens.concat(doubleOnes);
+      return results;
+      alert(results);
+    }
+    else if (inputArray.length === 3) //three characters long
+    {
+      for (var index = 0; index < inputArray.length; index++)
+      {
+        if (index === 0)
+        {
+          var tripleHundos = hundosArray[(inputArray[0])];
+        }
+        else if (index === 1)
+        {
+          var tripleTens = tensArray[(inputArray[1])];
+        }
+        else if (index === 2)
+        {
+          var tripleOnes = onesArray[(inputArray[2])];
+        }
+      }
+      var preResult = tripleHundos.concat(tripleTens);
+      var results = preResult.concat(tripleOnes);
+      return results;
+      alert(results);
+    }
+    else if (inputArray.length === 4) //four characters long
+    {
+      for (var index = 0; index < inputArray.length; index++)
+      {
+        if (index === 0)
+        {
+          var quadThousands = thousandsArray[(inputArray[0])];
+        }
+        else if (index === 1)
+        {
+          var quadHundos = hundosArray[(inputArray[1])];
+        }
+        else if (index === 2)
+        {
+          var quadTens = tensArray[(inputArray[2])];
+        }
+        else if (index === 3)
+        {
+          var quadOnes = onesArray[(inputArray[3])];
+        }
+      }
+      var prePreResults = quadThousands.concat(quadHundos);
+      var preResult = prePreResults.concat(quadTens);
+      var results = preResult.concat(quadOnes);
+      return results;
+      alert(results);
+    }
+  }
+  else
+  {
+    var results = "STOP!!!!!!";
     return results;
   }
 
